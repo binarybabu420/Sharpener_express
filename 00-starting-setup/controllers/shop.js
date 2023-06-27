1c1285dbc92aed142b6c5caef2b929a8e92c9463
binarybabu420 <sushant09450@gmail.com>
2023-06-27 20:25:55 +0530

    Dynnamic route part2

--- 00-starting-setup/controllers/shop.js
+++ 00-starting-setup/controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
-
+//const cart=require('../models/cart');
+const Cart = require('../models/cart');
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -23,9 +24,13 @@ exports.getIndex = (req, res, next) => {
 exports.getProduct=(req,res,next)=>{
   const prodId=req.params.productId;
   Product.findByid(prodId,product=>{
-    console.log(product);
+    res.render('shop/product-detail',{
+      product:product,
+      pageTitle:product.title,
+      path:'/products'
+    })
   })
-  res.redirect('/');
+  //res.redirect('/');
 
 };
 exports.getCart = (req, res, next) => {
@@ -34,6 +39,17 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart=(req,res,next)=>{
+  const prodId=req.body.productId;
+  Product.findByid(prodId,product=>{
+    Cart.addProduct(prodId,product.price);
+
+  })
+  console.log(prodId);
+  res.redirect('/cart');
+
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
